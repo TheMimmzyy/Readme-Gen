75ccb878b83b6181282003f8eb63e202e5dc7ed9
TheMimmzyy <cjc3481@gmail.com>
2021-06-27 21:04:53 -0600

    wrote functions for user questions

--- index.js
+++ index.js
@@ -1,13 +1,85 @@
-// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// array of questions for user
+const questions = [
+  "What is the Title of your project?",
+  "What is a description of your project?",
+  "What are the installation instructions?",
+  "What is the projects usage information?",
+  "What License is application covered under?",
+  "Any contribution guidelines?",
+  "What are your test instructions?",
+  "What is your GitHub username?",
+  "What is your email address?",
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function to write README file
+function writeToFile(fileName, data) {
+  console.log(data);
+  const genMarkdown = generateMarkdown(data);
+  fs.writeFile(fileName, genMarkdown, (err) => {
+    if (err) throw err;
+  });
+}
 
-// TODO: Create a function to initialize app
-function init() {}
+// function to initialize program
+function init() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "title",
+        message: questions[0],
+      },
+      {
+        type: "input",
+        name: "description",
+        message: questions[1],
+      },
+      {
+        type: "input",
+        name: "installation",
+        message: questions[2],
+      },
+      {
+        type: "input",
+        name: "usage",
+        message: questions[3],
+      },
+      {
+        type: "list",
+        name: "badge",
+        message: questions[4],
+        choices: ["None", "MIT", "Apache", "GPL"],
+      },
+      {
+        type: "input",
+        name: "contributing",
+        message: questions[5],
+      },
+      {
+        type: "input",
+        name: "tests",
+        message: questions[6],
+      },
 
-// Function call to initialize app
+      {
+        type: "input",
+        name: "github",
+        message: questions[7],
+      },
+      {
+        type: "input",
+        name: "email",
+        message: questions[8],
+      },
+    ])
+    .then((answers) => {
+      writeToFile("README.md", answers);
+    });
+}
+
+// function call to initialize program
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,39 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
+// function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
+  ![GitHub](https://img.shields.io/github/license/${data.github}/${data.title})
+  ## Description <a name="description"></a>
+  - ${data.description}
+  ## Table of Contents
+  1. [Description] (#Description)
+  2. [Installation Instructions](#Installation)
+  3. [Usage Information](#usage)
+  4. [License](#license)
+  5. [Contribution Guideline](#contributing)
+  6. [Test Instructions](#test)
+  7. [Questions](#questions)
+  8. [Screenshots](#screenshots)
+ 
+ 
+  ### Installation Instructions <a name="installation"></a>
+  - ${data.installation}
+  ### Usage Information <a name="Usage"></a>
+  - ${data.usage}
+  ## License <a name="license"></a>
+  - This application is covered under ${data.badge} license. 
+  ## Contribution Guideline <a name="contribution"></a>
+  - ${data.contributing}
+  ## Test Instructions <a name="tests"></a>
+  - ${data.tests}
+  ## Questions <a name="questions"></a>
+  Any questions? Please Contact Below
+  - GitHub Link: 
+  [${data.github}](https://github.com/${data.github}) 
+  - Email: 
+  ${data.email}
+  ## Screenshots <a name="screenshots"></a>
+  ![](././screenshots/readmepic.png)
+  
 `;
 }
 
